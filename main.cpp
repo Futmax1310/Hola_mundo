e631b48e7a3973cdb8b5bafe01615c43a994fb12
Alejandro_Basallote <abasred1306@g.educaand.es>
2022-02-14 22:22:55 +0100

    Saludo_personal

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-using namespace std;
+clude <iostream>
+	using namespace std;
 int main() {
 	
 	string nombre;
 	
-    cout << "¿Cual es su nombre?"<<endl;
+	cout << "¿Cual es su nombre?"<<endl;
 	cin >> nombre;
 	cout << "Bienvenido," << nombre << endl;
-    return 0;
+	return 0;
 }
